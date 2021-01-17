30c630d7470c2db899113788c1350e4a74461a1c
Samuel Haile <samishken@gmail.com>
2021-01-16 22:00:44 -0500

    Update strings.go

--- strings.go
+++ strings.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	// This is an example of a string
+	//This is an example of a string
 	fmt.Println("Hello")
 
 	var ourString string
 	ourString = "This is a Test"
+
 	fmt.Println(ourString)
 
 	//This is an example of a Rune
@@ -15,8 +19,28 @@ func main() {
 	ourRune = 'g'
 	fmt.Printf("Type: %T, \v Value: %v\n", ourRune, ourRune)
 
-	// Example of needing special character
-	fmt.Println("Don't touch That, that's my food")
-	fmt.Println("She said, \"Thats my food too\"")
+	//Example of needing special characters
+	fmt.Println("Don't Touch That, tha's my food")
+	fmt.Println("She said, \"thats my food too\"")
 
+	Example of string literal
+	fmt.Println(`Hello my name is Aaron\n,
+				and I am the coolest person`)
+
+	//example of length
+	fmt.Println(len("Hello World"))
+
+	//String Formatting
+	name := 'A'
+	age := 30
+	fmt.Printf("Hello my name is %c and I am %d years old", name, age)
+
+	//String Method
+	//Uppercase all
+	fmt.Println(strings.ToUpper("Gopher dbhdbhbdwhbdwhj"))
+
+	//Example Split
+	s := "Why Are We Here"
+	sSplit := strings.Split(s, "W")
+	fmt.Println(sSplit)
 }
